59ec321cf35172d7607b98d0b4a8cffae7419943
RaneemAbdulazez <raneem97su@gmail.com>
2021-01-27 14:34:16 +0200

    lab js is edit again final with color

--- app.js
+++ app.js
@@ -7,41 +7,48 @@
 
 var advice1='python is best for you ';
 var advice2='html is best for you ';
+var techPerson
 
 
 alert ("you'e welcom to coding school") 
+
 var userName = prompt('whats you name?');
+console.log(userName);
 
 
-
-if (confirm("are you a tech person ?")==true)
+if (window.confirm("are you a tech person ?")==true)
 {
-    
+    techPerson=true
+    console.log("techPerson= "+techPerson)
     alert (advice1+userName)
+    
+    if (window.confirm("Do you want to go to python?")==true){
+        window.location.href = "https://capmohamad.github.io/lab03/m";
+        window.location.replace("https://capmohamad.github.io/lab03/m");}
+        
+    else{
+        alert("let's play a game , you right a color and we gonna change the page color  ")
+        color= prompt("choose a color")
+        document.body.style.backgroundColor = color;
+
+        
+    }    
+
 }
 else {
+    techPerson=false
+    console.log("techPerson= "+techPerson)
     alert (advice2+userName)
-    document.write('<h1>'+'NOTE: if you know html you should move to css '+userName+'</h1>');
+    if (window.confirm("Do you want to go to html?")==true){
+        window.location.href = "https://bayankhalil.github.io/coding-school-/";
+        window.location.replace("https://bayankhalil.github.io/coding-school-/");}
+        
+    else{
+        document.write('<h1>'+'NOTE: if you know html you should move to css '+userName+'</h1>');
+    }    
+   
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//document.write('<h3>' + message +'</h3>');
-//userName = prompt('whats you name?');
-//alert('hello ' +userName);
-//confirm 
