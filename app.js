2529fc39a1af3bb71cf138949efd0aecbb0a1c49
RaneemAbdulazez <raneem97su@gmail.com>
2021-01-27 13:53:00 +0200

    lab js is done

---
+++ app.js
@@ -0,0 +1,47 @@
+//Add at least 3 dynamic components to your web page. These can be based on user input, or other dynamic input data.
+
+//At least one dynamic component should be based on user input.
+
+//At least one dynamic component should use a conditional statement to determine the output.
+var userAge='';
+var techPerson='';
+var advice1='python is best for you ';
+var advice2='html is best for you ';
+var userPreference='';
+
+alert ("you'e welcom to coding school") 
+var userName = prompt('whats you name?');
+
+
+
+if (confirm("are you a tech person ?")==true)
+{
+    
+    alert (advice1+userName)
+}
+else {
+    alert (advice2+userName)
+    document.write('<h1>'+'NOTE: if you know html you should move to css '+userName+'</h1>');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document.write('<h3>' + message +'</h3>');
+//userName = prompt('whats you name?');
+//alert('hello ' +userName);
+//confirm 
